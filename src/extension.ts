79cedc551a370014a77dd4f8798670fdd57d8d37
Ch3ru09 <xliang0905@gmail.com>
2023-12-06 01:22:16 -0500

    tried to implement

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
-import { Hover, languages } from "vscode";
+import { Hover, languages, MarkdownString } from "vscode";
+const removeMd = require("remove-markdown");
 
 export function activate(context: vscode.ExtensionContext) {
   type ExpandRecursively<T> = T extends object
@@ -17,19 +18,71 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = languages.registerHoverProvider(
     ["typescript", "typescriptreact"],
     {
-      provideHover(document, position, token) {
-        const range = document.getWordRangeAtPosition(position);
-        const word = document.getText(range);
-
-        return new Hover({
-          language: "typescript",
-          value: word,
-        });
-      },
+      provideHover,
     }
   );
 
   context.subscriptions.push(disposable);
+}
+
+async function provideHover(
+  document: vscode.TextDocument,
+  position: vscode.Position,
+  token: vscode.CancellationToken
+) {
+  const word = await getPlainText(position);
+
+  if (!word) {
+    return;
+  }
+
+  return new Hover({
+    language: "typescript",
+    value: word,
+  });
+}
+
+async function getPlainText(
+  position: vscode.Position
+): Promise<string | undefined> {
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    return;
+  }
+  const selection = activeEditor.document.getWordRangeAtPosition(position);
+  if (!selection) {
+    return;
+  }
+
+  const hovers = await vscode.commands.executeCommand<Hover[]>(
+    "vscode.executeHoverProvider",
+    activeEditor.document.uri,
+    activeEditor.selection.active
+  );
+
+  const parts = hovers
+    ?.flatMap((hover) => hover.contents)
+    .map((content) => getMarkdownValue(content as MarkdownString))
+    .filter((content) => content.length > 0);
+
+  if (!parts?.length) {
+    return;
+  }
+
+  const markdown = parts.join("\n---\n");
+  return removeMarkdown(markdown);
+}
+
+function removeMarkdown(markdown: string) {
+  const targetLanguageRegExp = new RegExp(/```(type|java)script/);
+  if (targetLanguageRegExp.test(markdown)) {
+    return markdown.replace(targetLanguageRegExp, "").replace(/```/, "");
+  }
+  return removeMd(markdown);
+}
+
+function getMarkdownValue(content: MarkdownString): string {
+  return content.value;
 }
 
 // This method is called when your extension is deactivated
